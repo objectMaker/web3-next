8b92d70501a362fa344cf95b68312d23f1b9cf62
于邦飞 <528154320@qq.com>
2024-07-16 13:35:13 +0800

    add get blockchain number

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import {ethers,AbstractProvider,JsonRpcSigner} from "ethers"
 
 export default function Test() {
@@ -41,10 +41,28 @@ export default function Test() {
 		const address = await eth.signer?.getAddress?.()
 		setAccount(address || '')
 	}
+
+	const [blockNumber,setBlockNumber] = useState('')
+
+	async function handleGetBlockNumber() {
+		if(!eth.provider){
+			return confirm("you have not connected!")
+		}
+		//get account api
+		const number = await eth.provider?.getBlockNumber?.()
+		setBlockNumber(number + '')
+	}
 	
 	return <>
 		<div>account : {account}</div>
-		<button onClick={connect} className="mr-3">connect</button>
-		<button onClick={handleGetAccount}>setAccount</button>
+		<button onClick={connect} className="hover:scale-110 mr-3 border-2 p-1 transition-all ease-linear hover:bg-light-800 rounded-sm border-green-800">connect</button>
+		<button onClick={handleGetAccount} className="hover:scale-110 mr-3 border-2 p-1 transition-all ease-linear hover:bg-light-800 rounded-sm border-green-800">		setAccount
+		</button>
+		<div>
+		blockNumber : {blockNumber}
+		</div>
+		<button onClick={handleGetBlockNumber} className="hover:scale-110 mr-3 border-2 p-1 transition-all ease-linear hover:bg-light-800 rounded-sm border-green-800">		setBlockNumber
+		</button>
+		
 	</>
 }
